b704dc2f506dc7821dab421c087c3795a795219d
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-10 15:45:15 +0100

    Inserital'icona del gatto nell'html tramite jquery
    Inserital'icona del gatto nell'html tramite jquery

--- milestone1/icons.js
+++ milestone1/icons.js
@@ -3,70 +3,71 @@ $(document).ready(function () {
   const icons =  [
     {
     name : "gatto",
-    prefix :"",
-    type : "",
-    family : "",
+    prefix :"fas",
+    type : "fa",
+    family : "animali",
     },
     {
     name : "corvo",
-    prefix :"" ,
-    type : "",
-    family : "",
+    prefix :"fas" ,
+    type : "fa",
+    family : "animali",
     },
     {
     name : "cane",
-    prefix :"",
-    type : "",
-    family : "",
+    prefix :"fas",
+    type : "fa",
+    family : "animali",
     },
     {
     name : "colomba",
-    prefix :"",
-    type : "",
-    family : "",
+    prefix :"fas",
+    type : "fa",
+    family : "animali",
     },
     {
     name : "drago",
-    prefix :"",
-    type : "",
-    family : "",
+    prefix :"fas",
+    type : "fa",
+    family : "animali",
     },
     {
     name : "cavallo",
-    prefix :"",
-    type : "",
-    family : "",
+    prefix :"fas",
+    type : "fa",
+    family : "animali",
     },
     {
     name : "ippopotamo",
-    prefix :"",
-    type : "",
-    family : "",
+    prefix :"fas",
+    type : "fa",
+    family : "animali",
     },
     {
     name : "pesce",
-    prefix :"",
-    type : "",
-    family : "",
-    },
+    prefix :"fas",
+    type : "fa",
+    family : "animali",
+    }
 
-]
-  // Selezioniamo il container icons
+  ]
+  console.log(icons);
+
   
+  // Selezioniamo il container icons
+  const gatto = $('div.icons').html('<div><i class="fas fa-cat"></i><div class="title">CAT</div>');
+  //const corvo = $('div.icons').html('<div><i class="fas fa-crow"></i><div class="title">CROW</div>');
+  //const cane = $('div.icons').html('<div><i class="fas fa-dog"></i><div class="title">DOG</div>');
+  //const colomba = $('div.icons').html('<div><i class="fas fa-dove"></i><div class="title">DOVE</div>');
+  //const drago = $('div.icons').html('<div><i class="fas fa-dragon"></i><div class="title">DRAGON</div>');
+  //const cavallo = $('div.icons').html('<div><i class="fas fa-horse"></i><div class="title">HORSE</div>');
+  //const ippopotamo = $('div.icons').html('<div><i class="fas fa-hippo"></i><div class="title">HIPPO</div>');
+  //const pesce = $('div.icons').html('<div><i class="fas fa-fish"></i><div class="title">FISH</div>');
+
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
   
-
-
-
-
-
-
-
-
-
-
-
 });
+
   
   
   /* ---- FUNCTIONS ----*/
